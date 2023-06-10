e07de0ae2c4853e8ae0eb0ed9a739a26cc8249df
Jessica Yun <jessicasopark@gmail.com>
2023-06-10 15:45:39 -0700

    Added dotenv

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT, 
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
@@ -23,7 +23,12 @@ CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL
-    -- references employee table
+    role_id INT,
+    manager_id INT, 
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
